404261b63e829f713f18a53c0f6943d7b4a214be
JeremLeb <jeremie.lebel@proton.me>
2026-04-25 21:52:35 -0400

    commit

---
+++ addons/cartoon_cloud/scene.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var speed: float = 2.0
+@export var distance: float = 10.0  # how far it travels each side
+
+var direction: float = 1.0
+var start_x: float
+
+func _ready():
+	start_x = position.x
+
+func _process(delta):
+	position.x += speed * direction * delta
+	if position.x >= start_x + distance:
+		direction = -1.0
+	elif position.x <= start_x - distance:
+		direction = 1.0
