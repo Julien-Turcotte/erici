1eb497c5ade54077b98e0846cd03196c81e58691
JeremLeb <jeremie.lebel@proton.me>
2026-04-26 15:02:11 -0400

    message

--- textures/dfgqeraegrhregergè.gd
+++ textures/dfgqeraegrhregergè.gd
@@ -17,7 +17,6 @@ func _on_body_entered(body: Node3D) -> void:
 	
 
 
-func _on_body_shape_entered(body_rid: RID, body: Node3D, body_shape_index: int, local_shape_index: int) -> void:
-	print(body)
-	print(body_rid)
-	#get_tree().change_scene_to_file("res://textures/airport2.tscn")
+func _on_body_shape_entered(body_rid, body, body_shape_index, local_shape_index):
+	if body.is_in_group("player"):
+		get_tree().change_scene_to_file("res://textures/airport2.tscn")
