7cf825db365ade15cd7a96b53cd8fc3489f42b02
JeremLeb <jeremie.lebel@proton.me>
2026-04-26 01:53:49 -0400

    init

--- physical_bone_simulator_3d.gd
+++ physical_bone_simulator_3d.gd
@@ -3,7 +3,13 @@ extends PhysicalBoneSimulator3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	physical_bones_start_simulation()
+	var node = get_parent()
+	while node != null:
+		if node is Skeleton3D:
+			if node.has_method("physical_bones_start_simulation"):
+				node.physical_bones_start_simulation()
+			return
+		node = node.get_parent()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
